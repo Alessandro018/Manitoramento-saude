b5d60dd444edb55fb6223671122a5c359b72eae6
Alessandro018 <alessandrosilva325@gmail.com>
2019-10-26 20:51:13 -0300

    Migração de checkups

---
+++ database/migrations/2019_10_26_223534_checkup.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Checkup extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id')->foreign('user_id')->references('id')->on('users');
+            $table->dateTime('data_checkup');
+            $table->float('peso');
+            $table->float('altura');
+            $table->text('pressao_arterial');
+            $table->Integer('nivel_glicose');
+            $table->float('colesterol_LDL');
+            $table->float('colesterol_HDL');
+            $table->text('observacoes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkups');
+    }
+}
