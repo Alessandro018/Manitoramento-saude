8d95c5cf5cce9fd9f4300854988f8d3311e48a99
gabrielpessoa <gabrielpessoanascimento@gmail.com>
2019-10-27 00:48:32 -0300

    Cadastro de usuarios

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,42 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="exampleFormControlSelect1" class="col-md-4 col-form-label text-md-right">{{ __('Tipo sanguíneo') }}</label>
+
+                            <div class="col-md-6">
+                                <select class="form-control" id="exampleFormControlSelect1" name="tipo_sanguineo" selected="{{ old('tipo_sanguineo') }}" required>
+                                    <option disabled selected>Selecione</option>
+                                    <option>A-</option>
+                                    <option>A+</option>
+                                    <option>B-</option>
+                                    <option>B+</option>
+                                    <option>AB-</option>
+                                    <option>AB+</option>
+                                    <option>O-</option>
+                                    <option>O+</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="exampleFormControlSelect1" class="col-md-4 col-form-label text-md-right">{{ __('Tipo de conta') }}</label>
+
+                            <div class="col-md-6">
+                                <?php
+                                    if(old('tipo')=='doutor'){ ?>
+                                        <input type="radio" name="tipo" value="doutor" require checked><label for="exampleFormControlSelect1" class="col-md-4 col-form-label">Doutor</label>
+                                        <input type="radio" name="tipo" value="usuario" require><label for="exampleFormControlSelect1" class="col-md-4 col-form-label ">Usuário</label>
+                                    <?php }elseif(old('tipo')=='usuario'){ ?>
+                                        <input type="radio" name="tipo" value="doutor" require><label for="exampleFormControlSelect1" class="col-md-4 col-form-label ">Doutor</label>
+                                        <input type="radio" name="tipo" value="usuario" require checked><label for="exampleFormControlSelect1" class="col-md-4 col-form-label">Usuário</label>
+                                    <?php }else{ ?>
+                                        <input type="radio" name="tipo" value="doutor" require><label for="exampleFormControlSelect1" class="col-md-4 col-form-label ">Doutor</label>
+                                        <input type="radio" name="tipo" value="usuario" require><label for="exampleFormControlSelect1" class="col-md-4 col-form-label ">Usuário</label>
+                                    <?php } ?>
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
