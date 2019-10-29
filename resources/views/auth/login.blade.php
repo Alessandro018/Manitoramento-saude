76e1e2a4619dd51b011aa3a047bb401ba2b84173
Alessandro018 <alessandrosilva325@gmail.com>
2019-10-29 00:00:47 -0300

    Redirecionamento na página de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+@if(Auth::check())
+    <?php header('location: /checkup'); exit;?>
+@endif
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
